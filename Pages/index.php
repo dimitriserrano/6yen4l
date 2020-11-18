0e33277fb02b40e5ac581c79e58fdcb0f5f413f0
celian frasca <celian.fras@gmail.com>
2020-11-18 09:58:38 +0100

    Ydays

---
+++ Pages/index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+<head>
+  <title>6Y en 4L</title>
+  <link rel="stylesheet" href="../sources/style.css">
+  <link href="http://fonts.cdnfonts.com/css/circular-std-book" rel="stylesheet">
+</head>
+
+<body>
+
+    <figure>
+        <img src="../desert.png" alt="desert">
+        <figcaption>
+            <h1>6Y en 4L</h1>
+            <h2>Participant au 4L TROPHY 2022</h2>
+            <div class ="Button">
+            <a href="empty.html">Dossier de Sponsoring</a>
+            <div class="voiture">
+            <img src="../4L.png" alt="4L">
+            </div>
+            </div>
+        </figcaption>
+    </figure>
+
+<header>
+    
+<div class="imagetop">
+<a href="index.php"><img src="../logotmarron.png" alt="mon logo"></a>
+</div>
+
+<!-- Menu de navigation du site -->
+<nav class="navbar">
+  <a href="index.php" style="color: rgb(213, 194, 72);">Accueil</a>
+  <a href="reflexions.html">Le raid</a>
+  <a href="#programme">Le programme</a>
+  <a href="#lequipe">L'équipe</a>
+  <a href="liens.html">Blog</a>
+  <a href="#contact">Contact</a>
+  
+</nav>
+
+</header>
+
+<main>
+<!-- Contenu principal -->
+
+<div class ="blocblanc">
+<h1>Présentation du raid</h1>
+<section>
+<h5>Qu'est-ce que le 4L TROPHY ?</h5></br>
+
+</br>Le 4L Trophy est le plus grand raid humanitaire, solidaire et sportif d’Europe dédié aux étudiants de toute nationalité, âgée de 18 à 28ans ! Chaque année, ce n’est pas moins de 2400 jeunes qui partent à l’aventure sur un parcours de plus de 6000km à travers la France, l’Espagne et le Maroc, le tout à bord de la mythique 4L 
+</section>
+
+<section>
+Quel est le but ?</br>
+
+</br>Au volant de leur 4L les trophystes devront arpenter le désert afin d'acheminer des fournitures scolaires et sportives aux enfants les plus démunis grâce à l'association "Enfants du Désert". Mais c'est aussi 12 tonnes de denrées alimentaires récolter chaque années au départ, soit plus de 24 000 repas distribués à des familles françaises dans le besoin. 
+</section>
+
+<section>
+Quel sont nos ambitions ? </br>
+
+</br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae erat non lorem rutrum sagittis. Fusce tempus venenatis dolor, quis porttitor magna consequat eu. Integer ac aliquam nisl. Vivamus scelerisque elit in lorem cursus, quis egestas quam aliquet. Donec ac turpis et mi ultricies condimentum. Maecenas ac velit egestas ex imperdiet mattis. Pellentesque condimentum ex ipsum, eget suscipit nibh pellentesque eget. Integer tortor erat, dapibus at varius nec, pretium non nunc. Ut vestibulum nibh at mi scelerisque, vel efficitur est ultricies. Nulla tempor diam quis elit lacinia auctor. Donec et neque eros.
+</section>
+<div class ="image1">
+<img src="../pointy.jpg" alt="image">
+</div>
+</div>
+<div class ="bordermed">
+<div class ="media">
+<section>
+    Media </br>
+
+</br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae erat non lorem rutrum sagittis. Fusce tempus venenatis dolor, quis porttitor magna consequat eu. Integer ac aliquam nisl. Vivamus scelerisque elit in lorem cursus, quis egestas quam aliquet. Donec ac turpis et mi ultricies condimentum. Maecenas ac velit egestas ex imperdiet mattis. Pellentesque condimentum ex ipsum, eget suscipit nibh pellentesque eget. Integer tortor erat, dapibus at varius nec, pretium non nunc. Ut vestibulum nibh at mi scelerisque, vel efficitur est ultricies. Nulla tempor diam quis elit lacinia auctor. Donec et neque eros.
+</section>
+
+<section>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae erat non lorem rutrum sagittis. Fusce tempus venenatis dolor, quis porttitor magna consequat eu. Integer ac aliquam nisl. Vivamus scelerisque elit in lorem cursus, quis egestas quam aliquet. Donec ac turpis et mi ultricies condimentum. Maecenas ac velit egestas ex imperdiet mattis. Pellentesque condimentum ex ipsum, eget suscipit nibh pellentesque eget. Integer tortor erat, dapibus at varius nec, pretium non nunc. Ut vestibulum nibh at mi scelerisque, vel efficitur est ultricies. Nulla tempor diam quis elit lacinia auctor. Donec et neque eros.
+</section>
+
+<section>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae erat non lorem rutrum sagittis. Fusce tempus venenatis dolor, quis porttitor magna consequat eu. Integer ac aliquam nisl. Vivamus scelerisque elit in lorem cursus, quis egestas quam aliquet. Donec ac turpis et mi ultricies condimentum. Maecenas ac velit egestas ex imperdiet mattis. Pellentesque condimentum ex ipsum, eget suscipit nibh pellentesque eget. Integer tortor erat, dapibus at varius nec, pretium non nunc. Ut vestibulum nibh at mi scelerisque, vel efficitur est ultricies. Nulla tempor diam quis elit lacinia auctor. Donec et neque eros.
+</section>
+</div>
+</div>
+
+<div class="titrechiffre">
+<H2>QUELQUES CHIFFRES</H2>
+</div>
+
+<div class="white2">
+</div>
+
+<div class ="programmeu">
+<h3>Le programme</h3>
+<div id="programme"></div>
+
+<section>
+<h4>BIARRITZ :</h4> 
+Procedente igitur mox tempore cum adventicium nihil inveniretur, relicta ora maritima in Lycaoniam adnexam Isauriae se contulerunt ibique densis intersaepientes itinera praetenturis provincialium et viatorum opibus pascebantur. </br>
+
+<h4>ALGECIRAS :</h4> 
+Nemo quaeso miretur, si post exsudatos labores itinerum longos congestosque adfatim commeatus fiducia vestri ductante barbaricos pagos adventans velut mutato repente consilio ad placidiora deverti.</br>
+
+<h4>TANGER :</h4> 
+Has autem provincias, quas Orontes ambiens amnis imosque pedes Cassii montis illius celsi praetermeans funditur in Parthenium mare, Gnaeus Pompeius superato Tigrane regnis Armeniorum abstractas dicioni Romanae coniunxit.</br>
+
+<h4>Boulajoul :</h4> 
+Nemo quaeso miretur, si post exsudatos labores itinerum longos congestosque adfatim commeatus fiducia vestri ductante barbaricos pagos adventans velut mutato repente consilio ad placidiora deverti.</br>
+
+<h4>MERZOUGA :</h4> 
+Sed tamen haec cum ita tutius observentur, quidam vigore artuum inminuto rogati ad nuptias ubi aurum dextris manibus cavatis offertur, inpigre vel usque Spoletium pergunt. haec nobilium sunt instituta.</br>
+
+<h4>Marrakech :</h4> 
+Has autem provincias, quas Orontes ambiens amnis imosque pedes Cassii montis illius celsi praetermeans funditur in Parthenium mare, Gnaeus Pompeius superato Tigrane regnis Armeniorum abstractas dicioni Romanae coniunxit.</br>
+
+<div class ="image2">
+<img src="../map.png" alt="image">
+</div>
+</div>
+
+<div class ="help">
+<H3>Comment nous aider ?</H3>
+
+<div class="bloc1">
+<h4>Partenariat financier</h4>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae erat non lorem rutrum sagittis. Fusce tempus venenatis dolor, quis porttitor magna consequat eu. Integer ac aliquam nisl. Vivamus scelerisque elit in lorem cursus, quis egestas quam aliquet. Donec ac turpis et mi ultricies condimentum. Maecenas ac velit egestas ex imperdiet mattis. Pellentesque condimentum ex ipsum, eget suscipit nibh pellentesque eget. Integer tortor erat, dapibus at varius nec, pretium non nunc. Ut vestibulum nibh at mi scelerisque, vel efficitur est ultricies. Nulla tempor diam quis elit lacinia auctor. Donec et neque eros.
+</p>
+</div>
+
+<div class="bloc2">
+<h4>Dons, Mécénat</h4>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae erat non lorem rutrum sagittis. Fusce tempus venenatis dolor, quis porttitor magna consequat eu. Integer ac aliquam nisl. Vivamus scelerisque elit in lorem cursus, quis egestas quam aliquet. Donec ac turpis et mi ultricies condimentum. Maecenas ac velit egestas ex imperdiet mattis. Pellentesque condimentum ex ipsum, eget suscipit nibh pellentesque eget. Integer tortor erat, dapibus at varius nec, pretium non nunc. Ut vestibulum nibh at mi scelerisque, vel efficitur est ultricies. Nulla tempor diam quis elit lacinia auctor. Donec et neque eros.
+</p>
+</div>
+
+<div class="bloc3">
+<h4>Partenariat en nature</h4>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae erat non lorem rutrum sagittis. Fusce tempus venenatis dolor, quis porttitor magna consequat eu. Integer ac aliquam nisl. Vivamus scelerisque elit in lorem cursus, quis egestas quam aliquet. Donec ac turpis et mi ultricies condimentum. Maecenas ac velit egestas ex imperdiet mattis. Pellentesque condimentum ex ipsum, eget suscipit nibh pellentesque eget. Integer tortor erat, dapibus at varius nec, pretium non nunc. Ut vestibulum nibh at mi scelerisque, vel efficitur est ultricies. Nulla tempor diam quis elit lacinia auctor. Donec et neque eros.
+</p>
+</div>
+
+<div class ="Button2">
+    <a href="empty.html">Dossier de Sponsoring</a>
+</div>
+
+</div>
+</div>
+
+<H2>L'équipe 6Y en 4L</H2>
+<div id="lequipe"></div>
+
+<H2>Contactez-nous</H2>
+<div id="contact"></div>
+<FORM method="POST" action="formulaire.php">
+<INPUT type="text" name="name" size=30 placeholder="Nom"required></br>
+<INPUT type="text" name="firstname" size=30 placeholder="Prénom"required><br>
+<INPUT type="text" name="email" size=30 placeholder="E-Mail" required><br>
+<INPUT type="text" name="tel" size=30 placeholder="Numéro de téléphone (Facultatif)"><br>
+<textarea name="message" cols=30 rows=5 placeholder="Message"required></textarea><br>
+<INPUT type="submit" value="Envoyer !">
+</FORM>
+
+</main>
+<a href="#">haut de page</a>
+</body>
+</html>
